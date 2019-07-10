d8f69b755eae6aa47645e435400f46d6fcf9262d
tavisca-mrathore <52023294+tavisca-mrathore@users.noreply.github.com>
2019-07-10 10:51:49 +0530

    Tests added

--- BowlingBall.Tests/GameFixture.cs
+++ BowlingBall.Tests/GameFixture.cs
@@ -6,9 +6,67 @@ namespace BowlingBall.Tests
     public class GameFixture
     {
         [Fact]
-        public void DummyTest()
+        public void testGutterGame()
         {
-            // This is a dummy test that will always pass.
+            var game = new Game();
+            RollPins(game, 20, 0);
+            Assert.Equal(0, game.GetScore());
         }
+
+        [Fact]
+        public void testHittingOnePinPerRoll()
+        {
+            var game = new Game();
+            RollPins(game, 20, 1);
+            Assert.Equal(20, game.GetScore());
+        }
+
+        [Fact]
+        public void testOneSpare()
+        {
+            var game = new Game();
+            rollSpare(game);
+            game.Roll(3);
+            RollPins(game, 17, 0);
+            Assert.Equal(16, game.GetScore());
+        }
+
+        [Fact]
+        public void testOneStrike()
+        {
+            var game = new Game();
+            rollStrike(game);
+            game.Roll(3);
+            game.Roll(4);
+            RollPins(game, 16, 0);
+            Assert.Equal(24, game.GetScore());
+        }
+
+        [Fact]
+        public void testPerfectGame()
+        {
+            var game = new Game();
+            RollPins(game, 12, 10);
+            Assert.Equal(300, game.GetScore());
+        }
+
+        // ---------------------------------------------------------------------
+        private void rollSpare(Game game)
+        {
+            game.Roll(5);
+            game.Roll(5);
+        }
+
+        private void RollPins(Game game, int numberOfRolls, int pinsHitPerRoll)
+        {
+            for (int i = 0; i < numberOfRolls; i++)
+                game.Roll(pinsHitPerRoll);
+        }
+
+        private void rollStrike(Game game)
+        {
+            game.Roll(10);
+        }
+
     }
 }
